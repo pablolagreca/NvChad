03dff4f017c62d6d3f348283bd4cef334137f265
Pablo La Greca <pablo.lagreca@msartcontract.com>
2024-05-06 21:32:10 -0300

    removing mappings for tabs

--- lua/nvchad/mappings.lua
+++ lua/nvchad/mappings.lua
@@ -31,21 +31,6 @@ map("n", "[d", vim.diagnostic.goto_prev, { desc = "lsp prev diagnostic" })
 map("n", "]d", vim.diagnostic.goto_next, { desc = "lsp next diagnostic" })
 map("n", "<leader>q", vim.diagnostic.setloclist, { desc = "lsp diagnostic loclist" })
 
--- tabufline
-map("n", "<leader>b", "<cmd>enew<CR>", { desc = "buffer new" })
-
-map("n", "<tab>", function()
-  require("nvchad.tabufline").next()
-end, { desc = "buffer goto next" })
-
-map("n", "<S-tab>", function()
-  require("nvchad.tabufline").prev()
-end, { desc = "buffer goto prev" })
-
-map("n", "<leader>x", function()
-  require("nvchad.tabufline").close_buffer()
-end, { desc = "buffer close" })
-
 -- Comment
 map("n", "<leader>/", function()
   require("Comment.api").toggle.linewise.current()
